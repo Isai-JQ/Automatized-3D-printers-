0011d102883205e6d1c398d52bba1c236d4337d5
Isai-JQ <a00842786@tec.mx>
2026-03-29 18:10:58 -0600

    status, le puse para ver el status de las cosas

--- backend.py
+++ backend.py
@@ -18,20 +18,41 @@ app.add_middleware(
 printer = None # mantains the printer connection
 class ConnectRequest(BaseModel):
     ip: str 
-    acess_code: str
+    access_code: str
     serial: str
 
 @app.post("/connect")                                                  
 def connect(request: ConnectRequest):
     global printer
     try:
-        printer = bl.BambulabsPrinter(request.ip, request.acess_code, request.serial) #creates the printer object with your credentials
+        printer = bl.Printer(request.ip, request.access_code, request.serial) #creates the printer object with your credentials
         printer.connect()
         time.sleep(2)  # Wait for the connection to establish   
         return {"ok":True, "message": f"Connected to {request.ip} successfully!"}
     except Exception as e:                                                        
         return {"ok":False, "message": f"Failed to connect to {request.ip}!"}
     
-if __name__ == "__main__":
+@app.get("/status")
+def status():
+    global printer
+    if printer is None:
+        return {"ok": False, "message": "Not connected to any printer."}
+    try:
+        return {
+            "ok": True,
+            "status": printer.get_status(),
+            "nozzle_temp": printer.get_nozzle_temperature(),
+            "bed_temp": printer.get_bed_temperature(),
+            "print_progress": printer.get_print_progress(),
+            "remaining_time": printer.get_remaining_time(),
+            "layer": printer.get_current_layer(),
+            "total_layers": printer.get_total_layer(),
+            "filename": printer.get_gcode_filename(), 
+        }
+    except Exception as e:
+        return {"ok": False, "message": "Failed to retrieve printer status."}
+   
+if __name__ == "__main__": 
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
